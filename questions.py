02bfc5bcc2f309858ca1212e508773ea9cc15c91
Gianluca Piergiacomi <piergiacomig@gmail.com>
2026-03-25 15:10:11 -0300

    Sistema de puntaje: muestra los puntos del jugador al final del juego.

--- questions.py
+++ questions.py
@@ -17,6 +17,8 @@ attemps = 6
 print ("Bienvenido al Ahorcado!")
 print()
 
+puntaje = 0         #inicializacion de puntaje 
+
 while attemps > 0:
     #Mostrar progreso: letras adividads y guiones para las que faltan
     progress =""
@@ -29,6 +31,7 @@ while attemps > 0:
     #verificar si el jugador ya adivinó la palabra completa
     if "_" not in progress:
         print ("¡Ganaste!")
+        puntaje += 6      #si el jugador adivina la palabra completa, se le suman 6 puntos.
         break
 
     print (f"Intentos restantes: {attemps})")
@@ -40,8 +43,10 @@ while attemps > 0:
     if len(letter) != 1 or not letter.isalpha():    #si la extensión de la letra es diferente a 1 o si no es una letra del alfabeto, se muestra el mensaje. 
         print ("Entrada no válida. Por favor, ingresa solo una letra.")
         continue        #con el continue, el usuario no pierde intentos, sino que se le pide que reigrese una letra válida.
-    
+
     #va a continuar solamente si la entrada es una letra del abecedario.
+
+    #Consigna 2: mostrar puntaje del jugador. Incorrecta resta 1, palabra completa suma 6, perder deja puntaje en cero.
     if letter in guessed:
         print ("Ya usaste esa letra.")
     elif letter in word:
@@ -50,8 +55,13 @@ while attemps > 0:
     else:
         guessed.append(letter)
         attemps -= 1
+        puntaje -= 1        #resta 1 por cada letra incorrecta.
         print ("Esa letra no está en la palabra.")
     print()
 
 else:
     print (f"¡Perdiste! La palabra era: {word}")
+    puntaje = 0         #puntaje en cero si el jugador pierde.
+
+#puntaje al final de la partida.
+print (f"Tu puntaje final es: {puntaje}")
